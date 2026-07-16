50fc15099ca1744be8aaf26b845eba36e6620437
Russel Palad <ctwa.russelpalad.bsis2a@gmail.com>
2026-07-17 05:01:50 +0800

    Update HTML structure and title in index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Activity2</title>
+    <title>Activity1</title>
 </head>
 <body>
         <?php
