9e0c68383bd7a20895ab27f3083b06fa65edab84
Russel Palad <ctwa.russelpalad.bsis2a@gmail.com>
2026-07-17 05:01:20 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Activity2</title>
+</head>
+<body>
+        <?php
+            // Russel
+            /*
+               Name: Russel Palad
+               Age: 22
+               Address: Guiguinto, Bulacan
+               Hobbies: Playing online games, Watching Movies, Listening to Music
+               Course: Bachelor of Science in Information Systems
+               School: Bulacan Polytechnic College
+            */
+
+                $me = [
+                    
+                    "Name" => "Russel Palad",
+                    "Age" => 22,
+                    "Address" => "Guiguinto, Bulacan",
+                    "Hobbies" => ["Playing online games", "Watching Movies", "Listening to Music"],
+                    "Course" => "Bachelor of Science in Information Systems",
+                    "School" => "Bulacan Polytechnic College",
+                ];
+
+                    echo "<h1>My Information</h1>";
+                    echo "<p><strong>Name:</strong> " . $me["Name"] . "</p>";
+                    echo "<p><strong>Age:</strong> " . $me["Age"] . "</p>";
+                    echo "<p><strong>Address:</strong> " . $me["Address"] . "</p>";
+                    echo "<p><strong>Hobbies:</strong> " . implode(", ", $me["Hobbies"]) . "</p>";
+                    echo "<p><strong>Course:</strong> " . $me["Course"] . "</p>";
+                    echo "<p><strong>School:</strong> " . $me["School"] . "</p>";
+                    echo "<br></br>";
+                    echo "==============================================================================================================================================================================================================================";
+                    echo "<br></br>";
+
+                    print "I am " . $me["Name"] . ", " . $me["Age"] . " years old, I live in " . $me["Address"] . ", and my hobbies are " . implode(", ", $me["Hobbies"]) . ". I am currently studying " . $me["Course"] . " at " . $me["School"] . ".";
+        ?>
+
+
+</body>
+</html>
